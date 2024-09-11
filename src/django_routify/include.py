b65c80641930aacf7b7f11337dda9084915c301a
vitaliypopel <vitaliypopel14@gmail.com>
2024-09-11 10:09:34 +0300

    Imroved include_router function

--- src/django_routify/include.py
+++ src/django_routify/include.py
@@ -4,4 +4,10 @@ from .router import Router
 
 
 def include_router(router: Router) -> path:
-    return path(router.prefix, include(router.urls))
+    '''
+        Include router is a function that making include registered urls.
+        Returning path object which can be inserted into urlpatterns.
+        :param router: Router
+        :return: django.urls.path
+    '''
+    return path(router.prefix, include(router.urls, namespace=router.app_name))
