64614a4ee6a54ff8c3c90eef3618fa437df9320f
vitaliypopel <vitaliypopel14@gmail.com>
2024-09-21 18:29:19 +0300

    Changed type hint for include_router returning, now it is URLResolver

--- src/django_routify/include.py
+++ src/django_routify/include.py
@@ -1,14 +1,14 @@
-from django.urls import path, include
+from django.urls import path, include, URLResolver
 
 from .router import Router
 
 
-def include_router(router: Router) -> list[path]:
+def include_router(router: Router) -> URLResolver:
     '''
         Include router is a function that making include registered urls.
         Returning list of path which can be inserted into urlpatterns.
         :param router: Router
-        :return: list[django.urls.path]
+        :return: django.urls.URLResolver
     '''
     return path(
         router.prefix,
