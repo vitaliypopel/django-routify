c0327ab0b954e102705eb6f6cfbcffe0e2559026
vitaliypopel <vitaliypopel14@gmail.com>
2024-09-26 14:30:28 +0300

    Added two different url registration to tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,18 +1,43 @@
 import unittest
 
-from .urls import default_urlpatterns, routify_urlpatterns
+from .urls import (
+    default_with_trailing_urlpatterns,
+    routify_with_trailing_urlpatterns,
+    default_without_trailing_urlpatterns,
+    routify_without_trailing_urlpatterns,
+)
 
 
 class DjangoRoutifyTests(unittest.TestCase):
-    def test(self):
+    def test_urls_with_trailing_slash(self):
         # If default urlpatterns != routify urlpatterns test will be failed.
         for default_urls_obj, routify_urls_obj in zip(
-            default_urlpatterns,
-            routify_urlpatterns,
+            default_with_trailing_urlpatterns,
+            routify_with_trailing_urlpatterns,
         ):
             for default_url, routify_url in zip(
                 default_urls_obj.url_patterns,
                 routify_urls_obj.url_patterns,
+            ):
+                self.assertEqual(
+                    str(default_url.pattern),
+                    str(routify_url.pattern),
+                )
+                self.assertEqual(
+                    default_url.pattern.name,
+                    routify_url.pattern.name,
+                )
+                # Checking if callbacks are the same is a bad idea,
+                # because objects cannot be the same
+
+    def test_urls_without_trailing_slash(self):
+        for default_urls_obj, routify_urls_obj in zip(
+                default_without_trailing_urlpatterns,
+                routify_without_trailing_urlpatterns,
+        ):
+            for default_url, routify_url in zip(
+                    default_urls_obj.url_patterns,
+                    routify_urls_obj.url_patterns,
             ):
                 self.assertEqual(
                     str(default_url.pattern),
--- tests/urls.py
+++ tests/urls.py
@@ -1,15 +1,22 @@
-# Default Django's url registration
+from django_routify import include_router
 from django.urls import path, include
-from .views import home, HomeRedirectView, HelloNameView
+from .views import (
+    home,
+    HomeRedirectView,
+    HelloNameView,
+    router_with_trailing,
+    routify_without_trailing,
+)
 
-default_urlpatterns = [
+# Default Django's url registration with trailing slash
+default_with_trailing_urlpatterns = [
     path(
-        'test/', # url prefix
+        'test/', # url prefix '<host:port>/test/'
         include((
             [
-                path('home/', home, name='home'),
-                path('', HomeRedirectView.as_view(), name='home_redirect'),
-                path('<slug:name>/', HelloNameView.as_view(), name='hello_name'),
+                path('home/', home, name='home'),                                   # '<host:port>/test/home/'
+                path('', HomeRedirectView.as_view(), name='home_redirect'),         # '<host:port>/test/'
+                path('<slug:name>/', HelloNameView.as_view(), name='hello_name'),   # '<host:port>/test/<slug:name>/'
             ], # urlpatterns
             'test', # app_name
         ))
@@ -17,10 +24,29 @@ default_urlpatterns = [
 ]
 
 
-# Django Routify url registration
-from django_routify import include_router
-from .views import router
+# Django Routify url registration with trailing slash
+routify_with_trailing_urlpatterns = [
+    include_router(router_with_trailing),
+]
 
-routify_urlpatterns = [
-    include_router(router),
+
+# Default Django's url registration with trailing slash
+default_without_trailing_urlpatterns = [
+    path(
+        'test', # url prefix '<host:port>/test'
+        include((
+            [
+                path('/home', home, name='home'),                                   # '<host:port>/test/home'
+                path('', HomeRedirectView.as_view(), name='home_redirect'),         # '<host:port>/test'
+                path('/<slug:name>', HelloNameView.as_view(), name='hello_name'),   # '<host:port>/test/<slug:name>'
+            ], # urlpatterns
+            'test', # app_name
+        ))
+    ),
+]
+
+
+# Django Routify url registration with trailing slash
+routify_without_trailing_urlpatterns = [
+    include_router(routify_without_trailing),
 ]
