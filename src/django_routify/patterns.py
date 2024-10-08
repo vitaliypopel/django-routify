6f93f4e3d5c0ea0052a6a8a97a94606d4c8532fd
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-08 15:59:13 +0300

    Implemented extra static methods in Pattern class

--- src/django_routify/patterns.py
+++ src/django_routify/patterns.py
@@ -22,11 +22,46 @@ class Pattern(BasePattern):
         class_based: bool,
     ) -> str:
         django_url = custom_url
-
         if self.REGEX == '':
             return django_url
 
         pass
+
+    @staticmethod
+    def _get_annotations(
+        view: Any,
+        class_based: bool,
+    ) -> dict[str, type]:
+        if class_based:
+            if hasattr(view, 'parameters'):
+                # getting annotations from attribute
+                return view.parameters
+            elif hasattr(view, 'get'):
+                # getting annotations from params in GET method
+                return view.get.__annotations__
+            elif hasattr(view, 'post'):
+                # getting annotations from params in POST method
+                return view.post.__annotations__
+            elif hasattr(view, 'put'):
+                # getting annotations from params in PUT method
+                return view.put.__annotations__
+            elif hasattr(view, 'patch'):
+                # getting annotations from params in PATCH method
+                return view.patch.__annotations__
+            elif hasattr(view, 'delete'):
+                # getting annotations from params in DELETE method
+                return view.delete.__annotations__
+        else:
+            # getting annotations from function params
+            return view.__annotations__
+
+    @staticmethod
+    def _get_django_type(python_type: type) -> str:
+        if python_type is int:
+            return 'int'
+        elif python_type is uuid.UUID:
+            return 'uuid'
+        return 'slug' # by default
 
 
 class ColonPattern(Pattern):
