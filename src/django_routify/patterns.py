2ed0648a8aca1d69dc9da35da2f88fc7df8a7cff
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-08 13:30:31 +0300

    Was made docstring for each of Patter based class

--- src/django_routify/patterns.py
+++ src/django_routify/patterns.py
@@ -1,18 +1,68 @@
+import re
+
+from rgx import REGEX
 from ._abstraction import BasePattern
 
 
 class Pattern(BasePattern):
+    '''
+    Pattern class is a default Pattern class
+
+    Attributes:
+        REGEX: str = r''
+    '''
+
+    REGEX = r''
+
     def normalize(self, custom_url: str) -> str:
+        django_url = custom_url
+
+        if REGEX == '':
+            return django_url
+
         pass
 
 
 class ColonPattern(Pattern):
+    '''
+    ColonPattern class for parsing colon based urls
+
+    Example:
+        /users/:id == /users/<int:id>
+        articles/:article/ == articles/<slug:article>/
+
+    Attributes:
+        REGEX: str = r':(\w+)'
+    '''
+
     REGEX = r':(\w+)'
 
 
 class CurlyPattern(Pattern):
+    '''
+    CurlyPattern class for parsing curly brackets based urls
+
+    Example:
+        /users/{id} == /users/<int:id>
+        articles/{article}/ == articles/<slug:article>/
+
+    Attributes:
+        REGEX: str = r'\{(\w+)\}'
+    '''
+
     REGEX = r'\{(\w+)\}'
 
 
 class AnglePattern(Pattern):
+    '''
+    AnglePattern class for parsing angle brackets based urls
+
+    Example:
+        /users/<id> == /users/<int:id>
+        articles/<article>/ == articles/<slug:article>/
+
+    Attributes:
+        REGEX: str = r'<(\w+)>'
+    '''
+
     REGEX = r'<(\w+)>'
